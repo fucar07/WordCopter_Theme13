cfff400cd53a40e540e005efd82fb7a72a31f9ec
Fatih UÇAR <fucar07@gmail.com>
2025-05-13 13:35:57 +0300

    Hata düzeltildi

--- scripts/c3runtime.js
+++ scripts/c3runtime.js
@@ -1298,15 +1298,8 @@ self.C3_ExpressionFuncs = [
 		},
 		() => "Sistem",
 		p => {
-			const f0 = p._GetNode(0).GetBoundMethod();
-			const n1 = p._GetNode(1);
-			const n2 = p._GetNode(2);
-			const n3 = p._GetNode(3);
-			return () => ((((f0(n1.ExpInstVar())) !== (0) ? 1 : 0)) ? (n2.ExpInstVar()) : (n3.ExpInstVar()));
-		},
-		p => {
-			const v0 = p._GetNode(0).GetVar();
-			return () => v0.GetValue();
+			const n0 = p._GetNode(0);
+			return () => n0.ExpInstVar();
 		},
 		p => {
 			const n0 = p._GetNode(0);
@@ -1316,18 +1309,15 @@ self.C3_ExpressionFuncs = [
 		() => 5,
 		() => 0.5,
 		p => {
-			const f0 = p._GetNode(0).GetBoundMethod();
-			const n1 = p._GetNode(1);
-			const f2 = p._GetNode(2).GetBoundMethod();
-			const n3 = p._GetNode(3);
-			return () => ((((f0(n1.ExpObject())) !== (0) ? 1 : 0)) ? (f2(n3.ExpObject())) : ((-1)));
-		},
-		p => {
 			const n0 = p._GetNode(0);
 			const v1 = p._GetNode(1).GetVar();
 			return () => n0.ExpObject((v1.GetValue() + 1));
 		},
 		() => "sure",
+		p => {
+			const v0 = p._GetNode(0).GetVar();
+			return () => v0.GetValue();
+		},
 		() => 3,
 		() => 7,
 		p => {
@@ -1358,11 +1348,6 @@ self.C3_ExpressionFuncs = [
 		p => {
 			const f0 = p._GetNode(0).GetBoundMethod();
 			return () => f0(0, 1);
-		},
-		p => {
-			const f0 = p._GetNode(0).GetBoundMethod();
-			const n1 = p._GetNode(1);
-			return () => f0(n1.ExpObject());
 		},
 		p => {
 			const v0 = p._GetNode(0).GetVar();
